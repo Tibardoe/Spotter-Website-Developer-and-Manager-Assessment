38d56c929aeff0be071aaae5915f5b230732c913
Tibardoe <tibardoebenjamin@gmail.com>
2025-08-04 18:58:06 +0100

    Initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        headerBg: "#051830",
+        headerBrightBg: "#40e0d04d",
+      },
+    },
+  },
+} satisfies Config;
